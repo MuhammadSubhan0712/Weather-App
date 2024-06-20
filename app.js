345da26c779335b3865fd09231e00f6436a6cb44
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-06-21 01:28:51 +0500

    Completed

--- app.js
+++ app.js
@@ -1,23 +1,18 @@
-
-
 const form = document.querySelector("#form");
 const input = document.querySelector("#input");
 const card = document.querySelector("#card");
 
-
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  // World wide weather Fake api 
+  // World wide weather Fake api
 
   axios(
     `https://api.weatherapi.com/v1/current.json?key=569c0fa5727f4e9387350542241406 &q=${input.value}&aqi=no`
   )
-
     .then((res) => {
-      
-  // Bootstrap card        
-        card.innerHTML += `
+      // Bootstrap card
+      card.innerHTML += `
     <div class="bg-dark card align-items-center border border-light rounded " style="width: 20%;">
   <img src="${res.data.current.condition.icon}" class="card-img-top" id="image" alt="icon">
   <div class="card-body">
@@ -28,11 +23,11 @@ form.addEventListener("submit", (event) => {
     <p class="card-text">Weather: ${res.data.current.condition.text}</p>
     <p class="card-text">Temperature: ${res.data.current.temp_c}°C</p>
   </div>
-</div>`
+</div>`;
       console.log(res.data);
     })
     .catch((err) => {
-        card.innerHTML = `${err}`
+      card.innerHTML = `${err}`;
       console.log(err);
     });
 });
