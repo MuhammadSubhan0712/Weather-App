b4fcfa9efb1c086312df9fe481a2fbaa9d4ad61a
M.Subhan Khan <MuhammadSubhan0712@gmail.com>
2024-06-21 00:58:09 +0500

    Completed

--- app.js
+++ app.js
@@ -11,14 +11,16 @@ form.addEventListener("submit", (event) => {
   )
     .then((res) => {
          
-        card.innerHTML = `
-    <div class="bg-dark card align-items-center " style="width: 20%;">
+        card.innerHTML += `
+    <div class="bg-dark card align-items-center border border-light rounded " style="width: 20%;">
   <img src="${res.data.current.condition.icon}" class="card-img-top" id="image" alt="icon">
   <div class="card-body">
     <h5 class="card-title">City: ${res.data.location.name} </h5>
-    <h6 class="card-title">Coutnry:  ${res.data.location.country} </h6>
+    <h6 class="card-title">Country:  ${res.data.location.country} </h6>
+    <h6 class="card-title">Region:  ${res.data.location.region} </h6>
     <p class="card-text">Local-Time: ${res.data.location.localtime}</p>
-    <p class="card-text">Weather: ${res.data.location.country}</p>
+    <p class="card-text">Weather: ${res.data.current.condition.text}</p>
+    <p class="card-text">Temperature: ${res.data.current.temp_c}°C</p>
   </div>
 </div>`
       console.log(res.data);
